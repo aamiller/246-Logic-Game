012db36d4810c2a78fec97668b0b48bcbbe8d5ce
aamiller <adelem@uw.edu>
2017-10-28 13:19:00 -0700

    Organized code, finished win condition edits, now working on graphics

--- js/index.js
+++ js/index.js
@@ -3,15 +3,18 @@
 let state = {
 	levelIsCompleted: [false, false, false, false, false, false],
 	timeElapsed: 0,
+	totalTimePaused: 0,
+	timePaused: 0,
+	isPaused: true,
+	timeBegan: 0,
 	numLevelsCompleted: 0,
 	currentLevel: 0,
 	levelsGuessCount: [0,0,0,0,0,0,0],
-	baseCases: ["2, 4, 6", "-1, 0, 1", "12, 16, 4", "4 4 4", "5 5 5", "6 6 6", "7 7 7"],
+	baseCases: ["2, 4, 6", "-1, 0, 1", "12, 16, 4", "2, 4, 8", "8, 1, 3", "4, 4, 4", "4, 2, 2"],
 	guessedTrueCasesPerLevel: [" ", " ", " ", " ", " ", " ", " "],
 	guessedFalseCasesPerLevel: [" ", " ", " ", " ", " "," ", " "],
 	currentGuessesOnTest: [false, false, false, false, false],
-	correctTestAnswers: [],
-	timeSpentPerLevelInSeconds: [0,0,0,0,0,0,0]
+	correctTestAnswers: []
 }
 
 /*
@@ -28,7 +31,7 @@ let state = {
 	// Increment guesses
 	state.levelsGuessCount[state.currentLevel]++;
 
-	let guessIsCorrect = testCase(var1, var2, var3);
+	let guessIsCorrect = testCase(parseInt(var1), parseInt(var2), parseInt(var3));
 	renderUserGuess(var1, var2, var3, guessIsCorrect);
 }
 
@@ -101,9 +104,14 @@ let state = {
  	levelChanged(6);	
  });
 
- function levelChanged(newLevel) {
- 	if (newLevel != state.currentLevel) {
- 		$( "#current-base-case" ).text(state.baseCases[newLevel]);
+/*
+* Changes the level to the input
+* level, changing the guessses the user has made
+* on that level as well.
+*/
+function levelChanged(newLevel) {
+	if (newLevel != state.currentLevel) {
+		$( "#current-base-case" ).text(state.baseCases[newLevel]);
 
 		// Save elements so that they can be replaced when switching between levels
 		state.guessedFalseCasesPerLevel[state.currentLevel] = document.querySelector('.false').innerHTML;
@@ -134,8 +142,11 @@ let state = {
 	// Generates 5 cases that the user can select as correct/incorrect
 
 	let levelTestCases = generateLevelTest();
+	state.correctTestAnswers = levelTestCases.caseIsCorrect;
+
 	let testHolder = $( ".testing-user-cases-container");
 
+	// Add each test case and a T/F button
 	levelTestCases.cases.forEach(function(currentValue, index) {
 		let newCase = $('<p class="guess"></p>');
 		newCase.text(currentValue);
@@ -164,6 +175,9 @@ let state = {
 $(document).on('click', '.tfButton', function() {
 	let isTrue = $(this).attr('tF');
 	let index = $(this).attr('index')
+
+	// Change the color of the output test on screen
+	// so the user can see what their guesses are.
 	if (isTrue == "true") {
 		$("p[numcase=" + index + "]").removeClass("wrong-guess").addClass("correct-guess");
 		state.currentGuessesOnTest[index] = true;
@@ -173,11 +187,10 @@ $(document).on('click', '.tfButton', function() {
 	}
 });
 
+// Submits and tests the users guesses
+// against the correct answers for that test.
 $(document).on('click', '#submit-test-answers', function() {
 	let allCorrect = true;
-	console.log("gen correct post click" + state.correctTestAnswers);
-	console.log("gen guesses" + state.currentGuessesOnTest);
-
 	state.correctTestAnswers.forEach(function(currentValue, index) {
 		if (currentValue != state.currentGuessesOnTest[index]) {
 			allCorrect = false;
@@ -195,12 +208,17 @@ $(document).on('click', '#submit-test-answers', function() {
 	}
 });
 
+
+/*
+* Marks level user is on as completed.
+*/ 
 function completedLevel() {
 	let level = state.currentLevel;
 	$( '#case-' + (level + 1) ).removeClass("btn-info").addClass("btn-success");
 	state.numLevelsCompleted++;
 	$( '#case-completed' ).text("Cases Completed: " + state.numLevelsCompleted)
 	state.levelIsCompleted[level] == true;
+	if (state.numLevelsCompleted == 7) { gameWon(); }
 
 	// Reset interface, clearing tests and re-showing containers
 	$( '.form-group, .guesses-container' ).show();
@@ -208,14 +226,14 @@ function completedLevel() {
 
 	// Switch to next level
 	if (state.currentLevel < 6) {
-		console.log("Change");
 		levelChanged(state.currentLevel + 1);
 	} else {
 	// Some congrats for finishing all the levels
+	}
 }
 
-}
-
+// If answers incorrect, asks user if they want to try again.
+// If not, hides the user testing part of the game.
 function failedLevel() {
 	if (confirm("Your guesses were incorrect. Want to change your answers?")) {
 		// Do nothing, let them change their answers
@@ -271,41 +289,56 @@ function failedLevel() {
  */
  function testLevelThreeInputs(var1, var2, var3) {
  	return (var1 + '').indexOf('6') > -1 |
- 			(var2 + '').indexOf('6') > -1 |
- 			(var3 + '').indexOf('6') > -1;
+ 	(var2 + '').indexOf('6') > -1 |
+ 	(var3 + '').indexOf('6') > -1;
  }
 
  /*
  * Level 4:
- * Must include an even number.
+ * Last number must be an even number.
  */
  function testLevelFourInputs(var1, var2, var3) {
- 	return var3 > var2 && var2 > var1;
+ 	return var3 % 2 == 0;
  }
 
  /*
 
  * Level 5:
- * Numbers must be increasing.
+ * A number includes a closed circle.
  */
  function testLevelFiveInputs(var1, var2, var3) {
- 	return var3 > var2 && var2 > var1;
+ 	return containsClosedCircleNum(var1) | 
+ 	containsClosedCircleNum(var2) |
+ 	containsClosedCircleNum(var3);
  }
+
+// Helper function that returns true
+// if closed circle numbers present.
+function containsClosedCircleNum(varInput) {
+	let variable = varInput + "";
+	return variable.indexOf("8") != -1 |
+	variable.indexOf("0") != -1 |
+	variable.indexOf("9") != -1 |
+	variable.indexOf("6") != -1;
+}
 
  /*
  * Level 6:
- * Numbers must be increasing.
+ * Numbers must all have integer square roots.
  */
  function testLevelSixInputs(var1, var2, var3) {
- 	return var3 > var2 && var2 > var1;
+ 	return Number.isInteger(Math.sqrt(var1)) &&
+ 	Number.isInteger(Math.sqrt(var2)) &&
+ 	Number.isInteger(Math.sqrt(var3));
  }
 
  /*
  * Level 7:
- * Numbers must be increasing.
+ * Numbers must sum to 0
+ * where the first number is negated.
  */
  function testLevelSevenInputs(var1, var2, var3) {
- 	return var3 > var2 && var2 > var1;
+ 	return (-1*var1 + var2 + var3) == 0;
  }
 
 /*
@@ -316,36 +349,53 @@ function failedLevel() {
 * Note: not all can use generateTest
 * because random test creation is too memory intensive.
 */
- function generateLevelTest() {
- 	if (state.currentLevel == 0) {
- 		return generateTest(testLevelOneInputs);
- 	} else if (state.currentLevel == 1) {
- 		return {
- 		 cases: ["0, 1, 2", 
- 		 		 "-2, 123, -3", 
- 		 		 "-3, -10, -84",
- 		 		 "81, 9, 3",
- 		 		 "12, -18, 4"],
- 		 caseIsCorrect: [false, true, true, false, true]};
- 	} else if (state.currentLevel == 2) {
- 		return {
- 		 cases: ["18, 90, 16", 
- 		 		 "-6, 45, -1", 
- 		 		 "3, 3, 8",
- 		 		 "-1, 75, 88",
- 		 		 "19, 0, true"],
- 		 caseIsCorrect: [true, true, false, false, false]};
- 	} else if (state.currentLevel == 3) {
- 		return generateTest(testLevelFourInputs);
- 	} else if (state.currentLevel == 4) {
- 		return generateTest(testLevelFiveInputs);
- 	} else if (state.currentLevel == 5) {
- 		return generateTest(testLevelSixInputs);
- 	} else if (state.currentLevel == 6) {
- 		return generateTest(testLevelSevenInputs);
-
- 	}
- }
+function generateLevelTest() {
+	if (state.currentLevel == 0) {
+		return generateTest(testLevelOneInputs);
+	} else if (state.currentLevel == 1) {
+		return {
+			cases: ["0, 1, 2", 
+			"-2, 123, -3", 
+			"-3, -10, -84",
+			"81, 9, 3",
+			"12, -18, 4"],
+			caseIsCorrect: [false, true, true, false, true]};
+		} else if (state.currentLevel == 2) {
+			return {
+				cases: ["18, 90, 16", 
+				"-6, 45, -1", 
+				"3, 3, 8",
+				"-1, 75, 88",
+				"19, 0, 1"],
+				caseIsCorrect: [true, true, false, false, false]};
+			} else if (state.currentLevel == 3) {
+				return generateTest(testLevelFourInputs);
+			} else if (state.currentLevel == 4) {
+				return {
+					cases: ["18, 90, 16", 
+					"-6, 45, -1", 
+					"3, 3, 1",
+					"-1, 75, 88",
+					"19, 0, 11"],
+					caseIsCorrect: [true, true, false, true, true]};
+				} else if (state.currentLevel == 5) {
+					return {
+						cases: ["18, 90, 16", 
+						"-6, 45, -1", 
+						"64, 2, 36",
+						"36, 4, 144",
+						"19, 0, 11"],
+						caseIsCorrect: [false, false, false, true, false]};
+					} else if (state.currentLevel == 6) {
+						return {
+							cases: ["4, 2, 2", 
+							"39, 13, 26", 
+							"-9, -3, -6",
+							"13, 4, 92",
+							"19, 0, 11"],
+							caseIsCorrect: [true, true, true, false, false]};
+						}
+					}
 
 /*
 * Takes in a function and creates
@@ -363,21 +413,94 @@ function generateTest(testLevelInputs) {
 
 	for (var i = 0; i < 5; i++) {
 		var needTrueCase = false;
- 		// Introduces a 1/10 chance of having a negative #
- 		let var1 = Math.floor((Math.random() - .3) * 100);
- 		let var2 = Math.floor((Math.random() - .3) * 100);
- 		let var3 = Math.floor((Math.random() - .3) * 100);
- 		let testPasses = testLevelInputs(var1, var2, var3);
- 		testArray.cases.push(var1 + ", " + var2 + ", " + var3);
- 		testArray.caseIsCorrect[i] = testPasses;
- 	}
+		// Introduces a 1/10 chance of having a negative #
+		let var1 = Math.floor((Math.random() - .3) * 100);
+		let var2 = Math.floor((Math.random() - .3) * 100);
+		let var3 = Math.floor((Math.random() - .3) * 100);
+		let testPasses = testLevelInputs(var1, var2, var3);
+		testArray.cases.push(var1 + ", " + var2 + ", " + var3);
+		testArray.caseIsCorrect[i] = testPasses;
+	}
 
 	// DOES NOT have a true and a false case, try again
 	// This does not recurse to avoid causing a stack error
-	if (testArray.caseIsCorrect.indexOf(true) == -1 || testArray.caseIsCorrect.indexOf(false) == -1) {
-		generateTest(testLevelInputs);
+	if (testArray.caseIsCorrect.indexOf(true) == -1 | testArray.caseIsCorrect.indexOf(false) == -1) {
+		return generateTest(testLevelInputs);
 	}
 
-	state.correctTestAnswers = testArray.caseIsCorrect;
 	return testArray;
+}
+
+
+
+/*** Timer code ***/
+// On the first click of the first input box, 
+// the timer starts.
+$( document.body ).one("click", function () {
+	if (state.isPaused) {
+		state.isPaused = false;
+	}
+	$( '#pause-button' ).text('Pause Timer');
+
+});
+
+$( "#pause-button" ).click(function () {
+	if (!state.isPaused) {
+		state.isPaused = true;
+		state.timePaused = Date.now();
+		$( '#pause-button' ).text('Unpause');
+	} else {
+		state.isPaused = false;
+		if (state.timePaused != 0) {
+			$( '#pause-button' ).text('Pause Timer');
+			state.totalTimePaused = state.totalTimePaused + (Date.now() - state.timePaused);
+		}
+	}
+});
+
+window.setInterval(updateTimer, 1000);
+
+/*
+* Gets difference in milliseconds between
+* time started and time ended.
+*/
+function updateTimer() {
+	if (state.isPaused == false) {
+		if (state.timeBegan == 0) { state.timeBegan = new Date(); }
+		state.timeElapsed = (new Date()) - state.timeBegan - state.totalTimePaused;
+		$ ( '#time' ).text("Time Playing: " + Math.floor((state.timeElapsed / 1000) / 60) + ":" + Math.floor(state.timeElapsed / 1000)%60);
+	}
+}
+/*** End timer code ***/
+
+
+
+/*
+* Executes when game is won.
+* Resets variables in the state and display.
+*
+*/
+function gameWon() {
+	state.levelIsCompleted = [false, false, false, false, false, false];
+	state.timeElapsed = 0;
+	state.totalTimePaused = 0;
+	state.timePaused = 0;
+	state.isPaused = false;
+	state.timeBegan =  0;
+	state.numLevelsCompleted = 0;
+	state.currentLevel =  0;
+	state.levelsGuessCount = [0,0,0,0,0,0,0];
+	state.guessedTrueCasesPerLevel = [" ", " ", " ", " ", " ", " ", " "];
+	state.guessedFalseCasesPerLevel = [" ", " ", " ", " ", " "," ", " "];
+	$( '#case-completed' ).text("Cases Completed: " + state.numLevelsCompleted);
+
+	for (var level = 0; level < 7; level++) {
+		$( '#case-' + (level + 1) ).removeClass("btn-success").addClass("btn-info");
+	}
+	document.querySelector('.false').innerHTML = "";
+	document.querySelector('.correct').innerHTML = "";
+
+	levelChanged(0);
+
+	$('#winModal').modal('show');
 }
